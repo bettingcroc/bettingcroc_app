dde2b630b2714bef7758c39d338aa39737105ddc
achille1017 <achillemarseille@gmail.com>
2023-11-19 19:52:49 +0100

    scripts web3 error handling

--- scripts/betManagements/betCloser.js
+++ scripts/betManagements/betCloser.js
@@ -4,46 +4,57 @@ import model from '../model.js'
 import { Web3 } from 'web3';
 import HDWalletProvider from '@truffle/hdwallet-provider'
 
+function run() {
+    try {
 
-const provider = new HDWalletProvider(PRIVATE_KEY_CERATOR, NODE_URL_BSCTESTNET);
-const web3 = new Web3(provider);
-const multiBetContract = new web3.eth.Contract(multiBetABI, multiBetAddress);
+        const provider = new HDWalletProvider(PRIVATE_KEY_CERATOR, NODE_URL_BSCTESTNET);
+        const web3 = new Web3(provider);
+        const multiBetContract = new web3.eth.Contract(multiBetABI, multiBetAddress);
 
-multiBetContract.setConfig({ contractDataInputFill: "both" })
+        multiBetContract.setConfig({ contractDataInputFill: "both" })
 
 
-function betCloser() {
-    setTimeout(betCloser, 60000);
-    let date1 = Math.floor(new Date().getTime() / 1000);
-    let date2 = Math.floor((new Date().getTime() + 60000) / 1000);
-    let resultDB = model.get_BetBetween2dates(date1, date2);
-    let betsToClose = [];
-    for (let i = 0; i < resultDB.length; i++) {
-        betsToClose.push(resultDB[i]["betNumber"]);
+        function betCloser() {
+            setTimeout(betCloser, 60000);
+            let date1 = Math.floor(new Date().getTime() / 1000);
+            let date2 = Math.floor((new Date().getTime() + 60000) / 1000);
+            let resultDB = model.get_BetBetween2dates(date1, date2);
+            let betsToClose = [];
+            for (let i = 0; i < resultDB.length; i++) {
+                betsToClose.push(resultDB[i]["betNumber"]);
+            }
+            if (betsToClose.length > 0) {
+                console.log(betsToClose + " sent for closing on " + new Date().toLocaleDateString() + " at " + new Date().toLocaleTimeString());
+                multiBetContract.methods
+                    .closeBets(betsToClose)
+                    .send({ from: PUBLIC_KEY_CREATOR })
+                    .on('receipt', function (receipt) {
+                        let betsToClose = []
+                        let logs = receipt.logs.filter(log => log.address.toLowerCase() === multiBetAddress.toLowerCase());
+                        logs.forEach(log => {
+                            let res = web3.eth.abi.decodeLog(betClosedABI, log.data, log.topics)
+                            betsToClose.push(parseInt(res.betNumber))
+                        });
+                        model.closeBets(betsToClose)
+                        logBetCloser(`${betsToClose} closed on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`)
+                    })
+                    .on('error', function (error, receipt) {
+                        console.log("error tx");
+                    })
+            }
+            else {
+                logBetCloser(`no bet to close on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()} to ${new Date(new Date().getTime() + 60000).toLocaleTimeString()}`)
+            }
+        }
+
+
+        betCloser();
     }
-    if (betsToClose.length > 0) {
-        console.log(betsToClose + " sent for closing on " + new Date().toLocaleDateString() + " at " + new Date().toLocaleTimeString());
-        multiBetContract.methods
-            .closeBets(betsToClose)
-            .send({ from: PUBLIC_KEY_CREATOR })
-            .on('receipt', function (receipt) {
-                let betsToClose = []
-                let logs = receipt.logs.filter(log => log.address.toLowerCase() === multiBetAddress.toLowerCase());
-                logs.forEach(log => {
-                    let res = web3.eth.abi.decodeLog(betClosedABI, log.data, log.topics)
-                    betsToClose.push(parseInt(res.betNumber))
-                });
-                model.closeBets(betsToClose)
-                logBetCloser(`${betsToClose} closed on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`)
-            })
-            .on('error', function (error, receipt) {
-                console.log("error tx");
-            })
-    }
-    else {
-        logBetCloser(`no bet to close on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()} to ${new Date(new Date().getTime() + 60000).toLocaleTimeString()}`)
+    catch (e) {
+        console.log("error running")
+        console.log(e)
+        run()
     }
 }
 
-
-betCloser();
+run()
--- scripts/betManagements/betEnder.js
+++ scripts/betManagements/betEnder.js
@@ -4,95 +4,106 @@ import { Web3 } from 'web3';
 import HDWalletProvider from '@truffle/hdwallet-provider'
 import { logBetEnder } from "../logger.js";
 
-const provider = new HDWalletProvider(PRIVATE_KEY_ENDER, NODE_URL_BSCTESTNET);
-const web3 = new Web3(provider);
-const multiBetContract = new web3.eth.Contract(multiBetABI, multiBetAddress);
-const DELAY = 60000
+function run() {
+    try {
+        const provider = new HDWalletProvider(PRIVATE_KEY_ENDER, NODE_URL_BSCTESTNET);
+        const web3 = new Web3(provider);
+        const multiBetContract = new web3.eth.Contract(multiBetABI, multiBetAddress);
+        const DELAY = 60000
 
-multiBetContract.setConfig({ contractDataInputFill: "both" })
-
-
-async function betEnder() {
-    setTimeout(betEnder, DELAY);
-    let betsClosed = db_betEnder.get_betClosed();
-    console.log(betsClosed)
-    if (betsClosed.length > 0) {
-        createWinnersArray(betsClosed)
-            .then((winnerArray) => {
-                if (winnerArray[0].length > 0) {
-                    console.log(`${winnerArray[0]} sent for ending on with ${winnerArray[1]} on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`);
-                    endBetOnChain(winnerArray[0], winnerArray[1]);
-                }
-                else {
-                    logBetEnder(`${new Date().toLocaleDateString()} ${new Date(new Date().getTime() - 60000).toLocaleTimeString()} to ${new Date().toLocaleTimeString()} : no bet to End`)
-                }
-            })
-    }
-    else {
-        logBetEnder(`${new Date().toLocaleDateString()} ${new Date(new Date().getTime() - 60000).toLocaleTimeString()} to ${new Date().toLocaleTimeString()} : no bet to End`)
-    }
-}
+        multiBetContract.setConfig({ contractDataInputFill: "both" })
 
 
-async function endBetOnChain(betsToEnd, winnerBetsToEnd) {
-    console.log("trying to end bets on chain")
-    await multiBetContract.methods
-        .endBets(betsToEnd, winnerBetsToEnd)
-        .send({ from: PUBLIC_KEY_ENDER })
-        .on('receipt', function (receipt) {
-            db_betEnder.endBets(betsToEnd)
-            logBetEnder(`${new Date().toLocaleDateString()} ${new Date()} bets ${betsToEnd} ended with ${winnerBetsToEnd}`)
-        })
-        .on('error', function (error, receipt) {
-            console.log(`error tx ${error}`);
-        })
-}
-
-
-async function createWinnersArray(betsClosed) {
-    let betsToEnd = [];
-    let winnerBetsToEnd = [];
-    await new Promise(async next => {
-        for (let i = 0; i < betsClosed.length; i++) {
-            let betNumber = betsClosed[i]
-            let options = {
-                'method': 'GET',
-                'headers': {
-                    'x-rapidapi-key': API_KEY,
-                    'x-rapidapi-host': 'v1.basketball.api-sports.io'
-                }
+        async function betEnder() {
+            setTimeout(betEnder, DELAY);
+            let betsClosed = db_betEnder.get_betClosed();
+            console.log(betsClosed)
+            if (betsClosed.length > 0) {
+                createWinnersArray(betsClosed)
+                    .then((winnerArray) => {
+                        if (winnerArray[0].length > 0) {
+                            console.log(`${winnerArray[0]} sent for ending on with ${winnerArray[1]} on ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`);
+                            endBetOnChain(winnerArray[0], winnerArray[1]);
+                        }
+                        else {
+                            logBetEnder(`${new Date().toLocaleDateString()} ${new Date(new Date().getTime() - 60000).toLocaleTimeString()} to ${new Date().toLocaleTimeString()} : no bet to End`)
+                        }
+                    })
             }
-            let res = await fetch(URL_API_BASKETBALL + `?id=${db_betEnder.get_idAPI(betNumber)}`, options)
-            let data = await res.json()
-            let matchStatus = data.response[0].status.short
-            let scoreHome = data.response[0].scores.home.total
-            let scoreAway = data.response[0].scores.away.total
-            if (matchStatus === "FT" || matchStatus === "AOT") {
-                db_betEnder.update_score(betNumber, scoreHome, scoreAway)
-                if (scoreHome > scoreAway) {
-                    winnerBetsToEnd.push(0);
-                }
-                else {
-                    winnerBetsToEnd.push(1);
-                }
-                betsToEnd.push(betNumber);
-            }
-            else if (
-                matchStatus === "POST" || matchStatus === "CANC" || matchStatus === "SUSP" || matchStatus === "AWD" || matchStatus === "ABD"
-            ) {
-                db_betEnder.cancelBet(betNumber)
-            }
-            else if (matchStatus !== "NS") {
-                db_betEnder.update_score(betNumber, scoreHome, scoreAway)
+            else {
+                logBetEnder(`${new Date().toLocaleDateString()} ${new Date(new Date().getTime() - 60000).toLocaleTimeString()} to ${new Date().toLocaleTimeString()} : no bet to End`)
             }
         }
-        next()
-    })
-    if (betsToEnd.length > 0) {
-        console.log(betsToEnd, winnerBetsToEnd);
+
+
+        async function endBetOnChain(betsToEnd, winnerBetsToEnd) {
+            console.log("trying to end bets on chain")
+            await multiBetContract.methods
+                .endBets(betsToEnd, winnerBetsToEnd)
+                .send({ from: PUBLIC_KEY_ENDER })
+                .on('receipt', function (receipt) {
+                    db_betEnder.endBets(betsToEnd)
+                    logBetEnder(`${new Date().toLocaleDateString()} ${new Date()} bets ${betsToEnd} ended with ${winnerBetsToEnd}`)
+                })
+                .on('error', function (error, receipt) {
+                    console.log(`error tx ${error}`);
+                })
+        }
+
+
+        async function createWinnersArray(betsClosed) {
+            let betsToEnd = [];
+            let winnerBetsToEnd = [];
+            await new Promise(async next => {
+                for (let i = 0; i < betsClosed.length; i++) {
+                    let betNumber = betsClosed[i]
+                    let options = {
+                        'method': 'GET',
+                        'headers': {
+                            'x-rapidapi-key': API_KEY,
+                            'x-rapidapi-host': 'v1.basketball.api-sports.io'
+                        }
+                    }
+                    let res = await fetch(URL_API_BASKETBALL + `?id=${db_betEnder.get_idAPI(betNumber)}`, options)
+                    let data = await res.json()
+                    let matchStatus = data.response[0].status.short
+                    let scoreHome = data.response[0].scores.home.total
+                    let scoreAway = data.response[0].scores.away.total
+                    if (matchStatus === "FT" || matchStatus === "AOT") {
+                        db_betEnder.update_score(betNumber, scoreHome, scoreAway)
+                        if (scoreHome > scoreAway) {
+                            winnerBetsToEnd.push(0);
+                        }
+                        else {
+                            winnerBetsToEnd.push(1);
+                        }
+                        betsToEnd.push(betNumber);
+                    }
+                    else if (
+                        matchStatus === "POST" || matchStatus === "CANC" || matchStatus === "SUSP" || matchStatus === "AWD" || matchStatus === "ABD"
+                    ) {
+                        db_betEnder.cancelBet(betNumber)
+                    }
+                    else if (matchStatus !== "NS") {
+                        db_betEnder.update_score(betNumber, scoreHome, scoreAway)
+                    }
+                }
+                next()
+            })
+            if (betsToEnd.length > 0) {
+                console.log(betsToEnd, winnerBetsToEnd);
+            }
+            return [betsToEnd, winnerBetsToEnd];
+        }
+
+
+        betEnder();
     }
-    return [betsToEnd, winnerBetsToEnd];
+    catch (e) {
+        console.log("error running")
+        console.log(e)
+        run()
+    }
 }
 
-
-betEnder();
+run()
